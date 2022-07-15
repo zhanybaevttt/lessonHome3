c6315c1e7065dadd8c442e8a6d7b727e1b64657e
programist <zhanybaevttt@gmail.com>
2022-07-15 17:21:47 +0300

    first commit

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,2 +1,18 @@
-package com.company;public class Circle {
+package com.company;
+
+public class Circle {
+    private final static double Pi = 3.14;
+
+    public static void area(int a) {
+        System.out.println(Pi*(a*a));
+
+
+    }
+    public static void circumference(int b){
+        System.out.println(Pi*2*b);
+
+
+    }
+
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
-        
+Circle.area(14);
+Circle.circumference(14);
+
     }
 }
